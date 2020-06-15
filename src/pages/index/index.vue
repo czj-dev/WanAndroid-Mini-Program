9fb4b91f522c9855d67c6fdd404a8393d08cbbf9
mrchenzhaojun@gmail.com <mrchenzhaojun@gmail.com>
2020-06-15 17:47:03 +0800

    改为 uni-app 来编写

---
+++ src/pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.testingHttp()
+		},
+		methods: {
+			testingHttp(){
+				uni.request({url:"https://wanandroid.com/wxarticle/chapters/json",success:(res)=>{
+					console.log(res)
+				}})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
